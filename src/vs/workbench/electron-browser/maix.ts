552538b364682e764f3af32398df082784f4350a
Alinshans <13925588730@163.com>
2018-04-27 20:18:53 +0800

    test json.

--- src/vs/workbench/electron-browser/maix.ts
+++ src/vs/workbench/electron-browser/maix.ts
@@ -1,8 +1,14 @@
 'use strict';
 
 import 'vs/css!./media/maix';
+import * as nls from 'vs/nls';
 import { Builder, $ } from 'vs/base/browser/builder';
 import { ipcRenderer } from 'electron';
+import * as fs from 'fs';
+import * as path from 'path';
+import uri from 'vs/base/common/uri';
+
+const rootPath = path.dirname(uri.parse(require.toUrl('')).fsPath);
 
 const Identifiers = {
 	MAIX_SETTINGS_WINDOWS: 'maix.workbench.settings.windows',
@@ -16,9 +22,12 @@ export class MaixSettingsWindows {
 	private maskContainer: Builder;
 	private maixContainer: Builder;
 	private parentContainer: Builder;
+	private maixConfig: any;
 
 	constructor(
 	) {
+		const configFilePath = path.join(rootPath, 'src', 'vs', 'test.json');
+		this.maixConfig = JSON.parse(fs.readFileSync(configFilePath, { encoding: 'utf8' }));
 	}
 
 	public init(parent: Builder): void {
@@ -45,23 +54,26 @@ export class MaixSettingsWindows {
 	}
 
 	private createSettingsWindows(): void {
-		// windows
+		// mask layer windows
 		this.maskContainer = $().div({
 			'class': 'mask-layer',
 			id: Identifiers.MASK_LAYER,
 			style: {'-webkit-filter': 'blur(5px)', 'z-index': '0'}
 		}).appendTo(this.parentContainer);
+
+		// settings windows
 		this.maixContainer = $().div({
 			'class': 'maix-container',
 			id: Identifiers.MAIX_SETTINGS_WINDOWS
 		}).appendTo(this.parentContainer);
 
 		// title bar
+		let titlebarName = this.maixConfig.maixSettingsWindows.titlebarName;
 		$(this.maixContainer).div({
 			'class': 'maix-titlebar',
 			id: Identifiers.MAIX_SETTINGS_TITLEBAR,
 			role: 'contentinfo'
-		}).text("Settings");
+		}).text(nls.localize("maixTitlebar", titlebarName));
 
 		this.setHidden(true);
 	}
