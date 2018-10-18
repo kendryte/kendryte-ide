fedc17f6fb83a11edea1e6cf9e5fa72b3ff307a5
GongT <admin@gongt.me>
2018-10-18 18:02:32 +0800

    fix css path

--- src/vs/kendryte/vs/workbench/packageManager/electron-browser/mainPanelContribution.ts
+++ src/vs/kendryte/vs/workbench/packageManager/electron-browser/mainPanelContribution.ts
@@ -1,4 +1,4 @@
-import 'vs/css!./media/package-manager';
+import 'vs/css!vs/kendryte/vs/workbench/packageManager/browser/media/package-manager';
 import { Registry } from 'vs/platform/registry/common/platform';
 import { Extensions as ViewletExtensions, ViewletDescriptor, ViewletRegistry } from 'vs/workbench/browser/viewlet';
 import { IPackageRegistryService, PACKAGE_MANAGER_TITLE, PACKAGE_MANAGER_VIEWLET_ID as VIEWLET_ID } from 'vs/kendryte/vs/workbench/packageManager/common/type';
