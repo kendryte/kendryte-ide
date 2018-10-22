754e8dbefa4b2415eca8d217475bed697fc8b5c4
sbYm <gongteng524702837@gmail.com>
2018-10-22 18:33:03 +0800

    fix restore ^C logic

--- my-scripts/start-watch.sh
+++ my-scripts/start-watch.sh
@@ -15,11 +15,11 @@ ensure_node_modules_in_current_dir
 node build/lib/electron.js || ./node_modules/.bin/gulp electron
 
 if command -v stty &>/dev/null ; then
-	stty intr ^K
 	echo ""
 	echo -e '\e[1;5;38;5;14m!!! Press Ctrl+K instead of Ctrl+C to stop this. !!!\e[0m'
 	echo ""
 	ORIG=$(stty -a | grep -oE 'intr\s*=\s*[^;]+' | sed 's/=//g')
+	stty intr ^K
 	trap "stty ${ORIG}" EXIT INT TERM
 fi
 
