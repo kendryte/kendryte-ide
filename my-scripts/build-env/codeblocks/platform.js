4dd0a2f0e8f70477c68ab55d914f117bfc5e5e0e
sbYm <gongteng524702837@gmail.com>
2018-11-14 18:31:35 +0800

    remove js files

--- my-scripts/build-env/codeblocks/platform.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLATFORM_STR_WINDOWS = 'win32';
-exports.PLATFORM_STR_MAC = 'darwin';
-exports.PLATFORM_STR_LINUX = 'linux';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGxhdGZvcm0uanMiLCJzb3VyY2VSb290IjoiLi8iLCJzb3VyY2VzIjpbImJ1aWxkLWVudi9jb2RlYmxvY2tzL3BsYXRmb3JtLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQWEsUUFBQSxvQkFBb0IsR0FBRyxPQUFPLENBQUM7QUFDL0IsUUFBQSxnQkFBZ0IsR0FBRyxRQUFRLENBQUM7QUFDNUIsUUFBQSxrQkFBa0IsR0FBRyxPQUFPLENBQUMifQ==
--- my-scripts/publisher/release.json.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// https://s3.cn-northwest-1.amazonaws.com.cn/kendryte-ide/release/IDE.json
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicmVsZWFzZS5qc29uLmpzIiwic291cmNlUm9vdCI6Ii4vIiwic291cmNlcyI6WyJwdWJsaXNoZXIvcmVsZWFzZS5qc29uLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSwyRUFBMkUifQ==
