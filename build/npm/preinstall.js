168dfa5c2889f384d5958ecc1f05cb2da2c334b3
GongT <gongteng524702837@gmail.com>
2019-01-01 03:17:31 +0800

    ignore preinstall

--- build/npm/preinstall.js
+++ build/npm/preinstall.js
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+process.exit(0);
 
 let err = false;
 
